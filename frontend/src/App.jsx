db74d6028c806d30022c468cf188f9389a92cba2
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2022-12-26 05:22:58 -0500

    fitst commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>Vite + React</h1>
+
+    </div>
+  );
+}
+
+export default App;
