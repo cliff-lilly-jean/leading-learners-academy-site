ef26f6d22446f37e90b5659c0cee270d97518b45
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2022-12-27 04:13:46 -0500

    Updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
-import './App.css';
+import './App.scss';
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <h1>Vite + React</h1>
-
+      <div className="hero">
+        <div className="flex-content">
+          <div className="content__left">
+            <span className="sub-title">{/* Subhead (small, orange color) */}A place to flourish and grow</span>
+            <h1 className='title'>{/* Title */}Leading Learners Academy</h1>
+            <p className="copy">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis voluptatum magni cumque animi repudiandae suscipit eveniet quo ex cum minus.</p>
+            <button className='cta'>
+              Get Started
+            </button>
+          </div>
+          <div className="content__right">
+            <img src="./assets/images/Home/header-baby.png" alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
