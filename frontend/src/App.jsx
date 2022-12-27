17e996f2e6685b8d0ed52ec639144140b3788ddc
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2022-12-27 04:48:38 -0500

    hero section edits

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,30 @@
 import './App.scss';
+import heroKids from './assets/images/Home/header-baby.png';
+import cloud from './assets/images/Home/cloud-01.png';
+
 
 function App() {
 
   return (
-    <div className="App">
+    <div className="home">
       <div className="hero">
-        <div className="flex-content">
-          <div className="content__left">
-            <span className="sub-title">{/* Subhead (small, orange color) */}A place to flourish and grow</span>
-            <h1 className='title'>{/* Title */}Leading Learners Academy</h1>
-            <p className="copy">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis voluptatum magni cumque animi repudiandae suscipit eveniet quo ex cum minus.</p>
-            <button className='cta'>
+        <div className="hero-content">
+          <div className="hero-content__left">
+            <span className="hero-sub-title">{/* Subhead (small, orange color) */}A place to flourish and grow</span>
+            <h1 className='hero-title'>{/* Title */}Leading Learners Academy</h1>
+            <p className="hero-copy">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis voluptatum magni cumque animi repudiandae suscipit eveniet quo ex cum minus.</p>
+            <button className='hero-cta'>
               Get Started
             </button>
           </div>
           <div className="content__right">
-            <img src="./assets/images/Home/header-baby.png" alt="" />
+            <div className="hero-img">
+              <img src={heroKids} alt="" />
+            </div>
           </div>
+        </div>
+        <div className="cloud">
+          <img src={cloud} alt="" />
         </div>
       </div>
     </div>
